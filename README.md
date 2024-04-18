4ab45b594626508e04ec96e92f2af52014e707d7
Diego Nunes <nunesdiegond@gmail.com>
2024-04-18 20:49:08 -0300

    CRUD completo

---
+++ README.md
@@ -0,0 +1,1 @@
+# sistema_cadastral_basico_Java_spring_Mysql_JPA
--- src/main/java/com/loja/cafeteria/Controller/EstadoController.java
+++ src/main/java/com/loja/cafeteria/Controller/EstadoController.java
@@ -1,9 +1,13 @@
 package com.loja.cafeteria.Controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.spel.support.ReflectivePropertyAccessor.OptimalPropertyAccessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -21,6 +25,26 @@ public class EstadoController {
 		ModelAndView modelView = new ModelAndView("/administrativo/estados/cadastro");
 		modelView.addObject("estado", estado);
 		return modelView;
+	}
+
+	@GetMapping("/administrativo/estados/lista")
+	public ModelAndView listar() {
+		ModelAndView modelView = new ModelAndView("/administrativo/estados/lista");
+		modelView.addObject("ListaEstados", estadoRepositorio.findAll());
+		return modelView;
+	}
+
+	@GetMapping("/editarEstado/{id}")
+	public ModelAndView editar(@PathVariable("id") Long id) {
+		Optional<Estado> estado = estadoRepositorio.findById(id);
+		return cadastrar(estado.get());
+	}
+	
+	@GetMapping("/removerEstado/{id}")
+	public ModelAndView remover(@PathVariable("id") Long id) {
+		Optional<Estado> estado = estadoRepositorio.findById(id);
+		estadoRepositorio.delete(estado.get());
+		return listar();
 	}
 	
 	@PostMapping("/salvarEstado")

189a58262d9c29087884d54a6a7db018ef372dbd
Diego Nunes <nunesdiegond@gmail.com>
2024-04-19 14:52:07 -0300

    configurações ajustadas, layout bootstrap, icones

--- src/main/java/com/loja/cafeteria/Controller/homeController.java
+++ src/main/java/com/loja/cafeteria/Controller/homeController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class homeController {
 	
-	@GetMapping("/administrativo")
-	public String acessarPrincipal() {
-		return "administrativo/home";
-	}
+    @GetMapping("/administrativo")
+    public String acessarPrincipal() {
+            return "administrativo/home";
+    }
 
 }
